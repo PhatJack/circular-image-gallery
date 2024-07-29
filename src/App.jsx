77c78ca83ad906d14a2e32ac1341ac253a70247f
PhatJack <tienphatng.693@gmail.com>
2024-07-29 17:11:14 +0700

    feat: add preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,115 @@
 import { useRef, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import { gsap } from "gsap";
 import "./App.css";
 import { useGSAP } from "@gsap/react";
+import Preload from "./Preload";
 
 function App() {
-  const container = useRef();
-  useGSAP(
-    () => {
-      const items = document.querySelectorAll(".item");
-      const itemList = gsap.utils.toArray(".item");
-      const container = document.querySelector(".container");
-      const numberOfItems = items.length;
-      const angleIncrement = (2 * Math.PI) / numberOfItems;
-      const radius = 300;
-      let isGalleryOpen = false;
+	const container = useRef();
+	useGSAP(
+		() => {
+			const items = document.querySelectorAll(".item");
+			const itemList = gsap.utils.toArray(".item");
+			const container = document.querySelector(".container");
+			const numberOfItems = items.length;
+			const angleIncrement = (2 * Math.PI) / numberOfItems;
+			const radius = 300;
+			let isGalleryOpen = false;
 
-      const centerX = container.offsetWidth / 2;
-      const centerY = container.offsetHeight / 2;
-      const tl = gsap.timeline();
+			const centerX = container.offsetWidth / 2;
+			const centerY = container.offsetHeight / 2;
+			const tl = gsap.timeline({ delay: 0.25 });
 
-      items.forEach((item, index) => {
-        const angle = index * angleIncrement;
-        const initialRotation = angle * (180 / Math.PI) + 90;
-        const x = centerX + radius * Math.cos(angle);
-        const y = centerY + radius * Math.sin(angle);
-        console.log(x, y);
-        gsap.set(item, { scale: 0 });
+			items.forEach((item, index) => {
+				const angle = index * angleIncrement;
+				const initialRotation = angle * (180 / Math.PI) + 90;
+				const x = centerX + radius * Math.cos(angle);
+				const y = centerY + radius * Math.sin(angle);
+				console.log(x, y);
+				gsap.set(item, { scale: 0 });
 
-        tl.to(
-          item,
-          {
-            left: x + "px",
-            top: y + "px",
-            scale: 1,
-            rotate: initialRotation,
-            duration: 1,
-            ease: "power2.out",
-            delay: 1,
-          },
-          index * 0.1
-        ).to(
-          ".gallery",
-          {
-            rotation: 360,
-			transformOrigin: "center center",
-            duration: 20,
-            repeat: -1,
-            ease: "none",
-          },
-          "<+=1.5"
-        );
-      });
-    },
-    {
-      scope: container.current,
-    }
-  );
+				tl.to(
+					item,
+					{
+						left: x + "px",
+						top: y + "px",
+						scale: 1,
+						rotate: initialRotation,
+						duration: 1,
+						ease: "power2.out",
+						delay: 1,
+					},
+					index * 0.1
+				)
+			});
+			tl.fromTo(
+				".name", {
+				y: 50,
+				scale: 0,
+			}, {
+				y: 0,
+				scale: 1,
+				duration: 1,
+				ease: "circ",
+			}, ">"
+			).from(".link", {
+				y: 50,
+				opacity: 0,
+				stagger: 0.1,
+				duration: 1,
+				ease: "circ",
+			}, "-=3").to(
+				".gallery",
+				{
+					rotation: 360,
+					transformOrigin: "center center",
+					duration: 20,
+					repeat: -1,
+					ease: "none",
+				},
+				">=2"
+			);
+		},
+		{
+			scope: container.current,
+		}
+	);
 
-  return (
-    <>
-      <nav>
-        <p>Jack Phat</p>
-        <ul>
-          <li className="link">
-            <a href="https://github.com/PhatJack" target="_blank">
-              github
-            </a>
-          </li>
-          <li className="link">
-            <a href="https://www.instagram.com/sep_neit.tahp/" target="_blank">
-              instagram
-            </a>
-          </li>
-          <li className="link">
-            <a href="https://www.facebook.com/jack.willam2003" target="_blank">
-              facebook
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <div className="container" ref={container}>
-        <div className="gallery">
-          {Array.from({ length: 8 }).map((_, i) => (
-            <div key={i} className="item">
-              <img src={`img${i + 1}.jpg`} alt="img" />
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<Preload />
+			<nav>
+				<p>Jack Phat</p>
+				<ul>
+					<li className="link">
+						<a href="https://github.com/PhatJack" target="_blank">
+							github
+						</a>
+					</li>
+					<li className="link">
+						<a href="https://www.instagram.com/sep_neit.tahp/" target="_blank">
+							instagram
+						</a>
+					</li>
+					<li className="link">
+						<a href="https://www.facebook.com/jack.willam2003" target="_blank">
+							facebook
+						</a>
+					</li>
+				</ul>
+			</nav>
+			<div className="container" ref={container}>
+				<div className="gallery">
+					{Array.from({ length: 8 }).map((_, i) => (
+						<div key={i} className="item">
+							<img src={`img${i + 1}.jpg`} alt="img" />
+						</div>
+					))}
+				</div>
+				<p className="name">Meiiii cute</p>
+			</div>
+		</>
+	);
 }
 
 export default App;
---
+++ src/Preload.jsx
@@ -0,0 +1,41 @@
+import React, { useRef } from 'react'
+import './Preload.css'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+const Preload = () => {
+
+	const preloadContainer = useRef()
+	useGSAP(() => {
+		const tl = gsap.timeline()
+		const items = document.querySelectorAll('.preload_col')
+		const itemList = gsap.utils.toArray('.preload_col')
+		tl.to(itemList, {
+			scaleY: 0,
+			transformOrigin: 'bottom',
+			stagger: {
+				amount: 0.5,
+				from: "center",
+			},
+		}).to(preloadContainer.current, {
+			autoAlpha: 0,
+			duration: 0.25,
+		})
+
+	}, {
+		scope: preloadContainer.current
+	})
+
+	return (
+		<div ref={preloadContainer} className="preload_container">
+			<div className="preload_group">
+				{
+					Array.from({ length: 20 }).map((_, i) => (
+						<div key={i} className="preload_col"></div>
+					))
+				}
+			</div>
+		</div>
+	)
+}
+
+export default Preload
